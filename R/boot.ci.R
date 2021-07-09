80fed2045197ddc7dd56c49113ccec5851b90ee2
Kaidi Kang <kaidikang@MacBook-Pro-4.local>
2021-07-09 18:04:18 -0500

    updated the output when model.reduced = NULL or not.

--- R/boot.ci.R
+++ R/boot.ci.R
@@ -17,6 +17,7 @@
 # 4. no sampling, just multipliers
 #' @param alpha significance level used to compute the CIs. By default, 0.05 will be used
 #' @param correct whether the residuals with bias correction will be used, by default, TRUE.
+#' @param num.cores The number of CPU cores to be used for calculating bootstrapped CIs, by default, only 1 core will be used.
 
 # Note:
 # 1. should we test the interaction terms?
@@ -31,25 +32,27 @@ boot.ci <- function(model.full, model.reduced = NULL, r = 1000, method = "F", mu
   # model forms
   form.full <- formula(model.full)
   if (is.null(model.reduced)){
-    model.reduced <- lm(as.formula(paste0(all.vars(form.full)[1], " ~ 1")), data = data)
+    form.reduced = NULL
+  } else {
+    form.reduced <- formula(model.reduced)
   }
-  form.reduced <- formula(model.reduced)
+
+
 
   # check whether the two models are nested
-  if (!(length((setdiff(all.vars(form.full), all.vars(form.reduced)))) > 0 &
-  length(setdiff(all.vars(form.reduced), all.vars(form.full))) == 0 )) {
-    stop("The models are not nested")
-  }
+  # if (!(length((setdiff(all.vars(form.full), all.vars(form.reduced)))) > 0 &
+  # length(setdiff(all.vars(form.reduced), all.vars(form.full))) == 0 )) {
+  #   stop("The models are not nested")
+  # }
 
   # covariance estimator
   if (tolower(method) == 'f'){ # assuming homoskedasticity
     vcovfunc <- vcov
   } else {
-    if (tolower(method) == "chisq"){ # using robust var-cov estimator
+    if (tolower(method) == "chisq"){ # using robust var-cov estimator (allowing heteroskedasticity)
       vcovfunc <- sandwich::vcovHC
     } else {stop("Please correctly specify the method to be used to compute the var-cov estimate (i.e., 'F' or 'Chisq')")}
   }
-
 
   S_hat = NULL
 
@@ -60,8 +63,8 @@ boot.ci <- function(model.full, model.reduced = NULL, r = 1000, method = "F", mu
     data$resid = residuals(model.full)
   }
 
-  # The variables of interest (difference between full & reduced models)
-  var.list <- setdiff(all.vars(form.full), all.vars(form.reduced))
+  # # The variables of interest (difference between full & reduced models)
+  # var.list <- setdiff(all.vars(form.full), all.vars(form.reduced))
 
   # bootstraps
   temp <- simplify2array(parallel::mclapply(1:r,
@@ -107,21 +110,21 @@ boot.ci <- function(model.full, model.reduced = NULL, r = 1000, method = "F", mu
                          ## full model
                          boot.model.full <- lm(form.full, data = boot.data)
                          ## reduced model
-                         if(!is.null(form.reduced)){
+                         if(!is.null(model.reduced)){
                            boot.model.reduced <- lm(form.reduced, data = boot.data)
                            # individual effects
-                           individual.anova <- suppressMessages(car::Anova(boot.model.full, test.statistics = "Wald", vcov. = vcovfunc))
-                           individual.stats <- individual.anova$F
-                           names(individual.stats) <- rownames(individual.anova)
-
-                           individual.stats <- individual.stats[var.list]
+                           # individual.anova <- suppressMessages(car::Anova(boot.model.full, test.statistics = "Wald", vcov. = vcovfunc))
+                           # individual.stats <- individual.anova$F
+                           # names(individual.stats) <- rownames(individual.anova)
+                           #
+                           # individual.stats <- individual.stats[var.list]
                            # overall effect
-                           overall.stats <- lmtest::waldtest(boot.model.reduced, boot.model.full, vcov =  vcovfunc(boot.model.full), test = "Chisq")$Chisq[2]
+                           overall.stats <- lmtest::waldtest(boot.model.reduced, boot.model.full, vcov =  vcovfunc, test = "Chisq")$Chisq[2]
                            names(overall.stats) <- "Overall"
-                           stats <- c(individual.stats, overall.stats)
+                           stats <- c(overall.stats)
                            stats
                          } else {
-                           boot.model.reduced <- lm(form.reduced, data = boot.data)
+                           boot.model.reduced <- lm(as.formula(paste0(all.vars(form.full)[1], " ~ 1")), data = boot.data) # y ~ 1
                            # directly compute the statistics based on full model
                            # individual effects
                            individual.anova <- suppressMessages(car::Anova(boot.model.full, test.statistics = "Wald", vcov. = vcovfunc))
@@ -137,19 +140,47 @@ boot.ci <- function(model.full, model.reduced = NULL, r = 1000, method = "F", mu
                        } # end of function in `lapply`
                        , boot.data = data, form.full = form.full, form.reduced = form.reduced, multi = multi, vcovfunc = vcovfunc, mc.cores = num.cores)
                       )
-  boot.stats = as.data.frame(t(temp))
+  temp2 = temp # temp still has the row names!
+  dim(temp2) = c(length(temp2)/r, r)
+  boot.stats = as.data.frame(t(temp2))
+  # use the row names of `temp`
+  colnames(boot.stats) = if(is.null(rownames(temp))) "Overall" else {rownames(temp)}
 
   # calculate the statistics for the data
-  ### individual effects
-  original.anova <- suppressMessages(car::Anova(model.full, test.statistics = "Wald", vcov. = vcovfunc))
-  original.anova <- original.anova[c(var.list, "Residuals"), ]
-  ### overall effect
-  original.overall <- lmtest::waldtest(model.reduced, model.full, vcov = vcovfunc, test = "Chisq")
+  if (!is.null(form.reduced)){
+    # ### individual effects
+    # original.anova <- suppressMessages(car::Anova(model.full, test.statistics = "Wald", vcov. = vcovfunc))
+    ### overall effect
+    original.overall <- lmtest::waldtest(model.reduced, model.full, vcov = vcovfunc, test = "Chisq")
+    ### d.f
+    dfs <- c(original.overall$Df[2], original.overall$Res.Df[2])
+    names(dfs) <- c("Overall", "Residuals")
+    # names(dfs) <- rownames(original.overall)
+    # overall.df <- sum(dfs[names(dfs) != "Residuals"])
+    # dfs <- c(dfs, Overall = overall.df)
+    anova.tab = data.frame(Df = dfs,
+                           Stat = c(original.overall$Chisq[2], NA),
+                           'P-value' = c(original.overall$`Pr(>Chisq)`[2], NA))
+  }  else {
+    ### individual effects
+    original.anova <- suppressMessages(car::Anova(model.full, test.statistics = "Wald", vcov. = vcovfunc))
+    ### overall effect
+    model.reduced <- lm(as.formula(paste0(all.vars(form.full)[1], " ~ 1")), data = data)
+    original.overall <- lmtest::waldtest(model.reduced, model.full, vcov = vcovfunc, test = "Chisq")
+    ### d.f
+    dfs <- original.anova$Df
+    names(dfs) <- rownames(original.anova)
+    overall.df <- original.overall$Df[2]
+    dfs <- c(dfs, Overall = overall.df)
 
-  dfs <- original.anova$Df
-  names(dfs) <- rownames(original.anova)
-  overall.df <- sum(dfs[names(dfs) %in% var.list])
-  dfs <- c(dfs, Overall = overall.df)
+    anova.tab = data.frame(Df = dfs,
+                           Stat = c(original.anova$F, original.overall$Chisq[2]),
+                           'P-value' = c(original.anova$`Pr(>F)`, original.overall$`Pr(>Chisq)`[2]))
+
+    # list of effects of interest
+    effect.list <- all.vars(form.full)[-1]
+    anova.tab = anova.tab[c(effect.list, "Overall", "Residuals"), ]
+  }
 
   ## conversion from stats to RESI
   ### define conversion function
@@ -174,15 +205,14 @@ boot.ci <- function(model.full, model.reduced = NULL, r = 1000, method = "F", mu
     # compute the quantiles for each column (i.e., for each effect)
     CIs = apply(boot.S, 2,  quantile, probs = c(alpha/2, 1-alpha/2))
     CIs = t(CIs)
-    Stats <- c(original.anova$F, original.overall$Chisq[2])
-    p_values <- c(original.anova$`Pr(>F)`, original.overall$`Pr(>Chisq)`[2])
-    Ss <- stat2S(Stats, dfs, dfs['Residuals'])
-    anova.tab <- data.frame(df = dfs, 'Stat' = Stats, 'P-value' = p_values, RESI = Ss)
-    # name list of effects
-    effect.list <- setdiff(all.vars(form.full), all.vars(form.reduced))
-    anova.tab <- anova.tab[c(effect.list, "Overall", "Residuals"), ]
-    anova.tab <- cbind(anova.tab, rbind(CIs[c(effect.list, "Overall"), ],  NA) )
-    colnames(anova.tab) = c("df", "Stat", "P value", "RESI", paste0(alpha/2*100, "% ", c("LB", "UB")) )
+    # Stats <- c(original.anova$F, original.overall$Chisq[2])
+    # p_values <- c(original.anova$`Pr(>F)`, original.overall$`Pr(>Chisq)`[2])
+    dfs = anova.tab$Df
+    names(dfs) = rownames(anova.tab)
+    Ss <- stat2S(anova.tab$Stat, dfs, dfs['Residuals'])
+    anova.tab <- cbind(anova.tab, RESI = Ss, rbind(CIs,  NA))
+
+    # colnames(anova.tab) = c("df", "Stat", "P value", "RESI", paste0(alpha/2*100, "% ", c("LB", "UB")) )
     output = list(model.full = form.full, model.reduced = form.reduced,
                   boot.type = boot.type, multiplier = multi, method = method,
                   alpha = alpha, correct = correct, anova = round(anova.tab, 3))
