a33639f2d6f0f3fe74d72a6797a75c5b9a2dc3ba
KaidiK <55763756+KaidiK@users.noreply.github.com>
2022-05-24 15:05:51 -0500

    rename `anoes` as `resi`

--- R/resi.R
+++ R/resi.R
@@ -15,8 +15,8 @@
 #   return(output)
 # }
 #
-anoes <- function(x, ...){
-  UseMethod("anoes")
+resi <- function(x, ...){
+  UseMethod("resi")
 }
 
 
@@ -28,13 +28,12 @@ anoes <- function(x, ...){
 #' @param nboot numeric, the number of bootstrap replicates. By default, 1000 bootstraps will be implemented.
 #' @param robust.var default to TRUE, whether to use the robust (sandwich) variance estimator when construct the Wald test statistic. If `TRUE`, the variance of the estimator will be obtained by using `sandwich::vcovHC()`` and the HC3 will be applied.
 #' @param multi the distribution from which the multipliers will be drawn: 'none' = the multipliers equal constant 1 (default); 'rad' = rademacher; 'normal' = Std Normal distribution
-#' @param boot.type which type of bootstrap to use. 1: resampling covariates along with residuals (default); 2: fixing covariates and only bootstrapping residulas; 3: resampling covariates and residuals independently w/ replacements; 4. no sampling, just multipliers
-#' @param alpha significance level of the constructed CIs. By default, 0.05 will be used0
-#' @param digits the number of decimal digits in the output ANOES table. By default, 3
+#' @param boot.method which type of bootstrap to use: `nonpara` = non-parametric bootstrap (default); `bayes` = Bayesian bootstrap.
+#' @param alpha significance level of the constructed CIs. By default, 0.05 will be used.
 #' @export
-anoes.glm <- function(object = NULL, model.full = NULL, model.reduced = NULL,
+resi.glm <- function(object = NULL, model.full = NULL, model.reduced = NULL,
                       robust.var = TRUE,
-                      boot.type = 1, multi = 'none',
+                      boot.method = "nonparametric",
                       nboot = 1000, alpha = 0.05){
   if (is.null(model.reduced)){
     output = resi.glm(object)
@@ -45,7 +44,7 @@ anoes.glm <- function(object = NULL, model.full = NULL, model.reduced = NULL,
       boot.data = boot.samp(data)
       # re-fit the model
       boot.mod = update(object, data = boot.data)
-      output.boot = cbind(output.boot, resi.glm(boot.mod)[, 'RESI'])
+      output.boot = cbind(output.boot, calc_resi.glm(boot.mod)[, 'RESI'])
     }
     output.boot = output.boot[, -1]
     RESI.ci = apply(output.boot, 1, quantile, probs = c(alpha/2, 1-alpha/2), na.rm = TRUE)
@@ -70,7 +69,7 @@ anoes.glm <- function(object = NULL, model.full = NULL, model.reduced = NULL,
 #' @param nboot numeric, the number of bootstraps used to construct CIs. By default, 1000.
 #' @export
 #' @return An ANOVA-type model summary output with RESI estimates and CIs added.
-anoes.geeglm <- function(object, alpha = 0.05, nboot = 1000){
+resi.geeglm <- function(object, alpha = 0.05, nboot = 1000){
   output = resi.geeglm(object) # RESI point estimates
   data = object$data
   # id variable name
@@ -81,7 +80,7 @@ anoes.geeglm <- function(object, alpha = 0.05, nboot = 1000){
     boot.data = boot.samp(data, id.var = id_var)
     # re-fit the model
     boot.mod = update(object, data = boot.data)
-    output.boot = cbind(output.boot, resi.geeglm(boot.mod)[, 'RESI'])
+    output.boot = cbind(output.boot, calc_resi.geeglm(boot.mod)[, 'RESI'])
   }
   output.boot = output.boot[, -1]
   RESI.ci = apply(output.boot, 1, quantile, probs = c(alpha/2, 1-alpha/2))
@@ -97,7 +96,7 @@ anoes.geeglm <- function(object, alpha = 0.05, nboot = 1000){
 #' @param nboot numeric, the number of bootstraps used to construct CIs. By default, 1000.
 #' @export
 #' @return An ANOVA-type model summary output with RESI estimates and CIs added.
-anoes.lme <- function(object, alpha = 0.05, nboot = 1000){
+resi.lme <- function(object, alpha = 0.05, nboot = 1000){
   output = resi.lme(object) # RESI point estimates
   data = object$data
   # id variable name
@@ -108,7 +107,7 @@ anoes.lme <- function(object, alpha = 0.05, nboot = 1000){
     boot.data = boot.samp(data, id.var = id_var)
     # re-fit the model
     boot.mod = update(object, data = boot.data)
-    output.boot = cbind(output.boot, resi.lme(boot.mod)[, 'RESI'])
+    output.boot = cbind(output.boot, calc_resi.lme(boot.mod)[, 'RESI'])
   }
   output.boot = output.boot[, -1]
   RESI.ci = apply(output.boot, 1, quantile, probs = c(alpha/2, 1-alpha/2))
