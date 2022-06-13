1d1b0e3c87d0fe72a69f9958a16d6b927e26ec77
MeganTJ21 <megan.n.taylor@vanderbilt.edu>
2022-06-13 17:15:32 -0500

    deleted duplicate functions

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -10,6 +10,8 @@ chisq2Ssq = function(chisq, df, rdf){
   S = (chisq - df)/rdf
 }
 
+#' Compute the robust effect size index estimator from chi-squared statistic.
+#'
 #' This function computes the robust effect size index from Vandekar, Rao, & Blume (2020).
 #' Vector arguments are accepted. If different length arguments are passed they are dealt with in the usual way of R.
 #' For mixed effects models, RESI is conditional on the average correlation
@@ -125,5 +127,20 @@ print.resi <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
   if(nzchar(mess <- naprint(x$na.action))) cat("  (",mess, ")\n", sep = "")
 
   invisible(x)
+}
+
+
+#' Convert S to Cohen's d
+#'
+#' Converts the robust effect size index to Cohen's d using formula from Vandekar, Rao, & Blume (2020).
+#' @param S The robust effect size index.
+#' @param pi The sampling proportions.
+#' @keywords power
+#' @return Returns an estimate the robust effect size index
+#' @details The pi parameter comes from the fact that Cohen's d doesn't account for unequal sample proportions in the population, but S does.
+#' The default is set to a natural value 1/2, which corresponds to a case control design, for example, where sampling proportions always are controlled by the experimenter.
+#' @export
+S2d = function(S, pi=0.5){
+  S * sqrt(1/pi + 1/(1-pi) )
 }
 
