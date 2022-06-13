cfdcba1de658f3220b8f8e008f2e5c3faa5b46cd
MeganTJ21 <megan.n.taylor@vanderbilt.edu>
2022-06-13 17:09:16 -0500

    typo fix

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,11 +1,11 @@
 # Internal functions
 
-#' Transferation from Wald test statistics to squared RESI
-#' @export
+#' Transformation from Wald test statistics to squared RESI
 #' @param chisq The chi-square statistic for the parameter(s) of interest.
 #' @param df Number of degrees of freedom of the chi-square statistic.
 #' @param rdf Model residual degrees of freedom or number of independent samples.
 #' @return Returns a scalar or vector argument of the squared robust effect size index estimate.
+#' @export
 chisq2Ssq = function(chisq, df, rdf){
   S = (chisq - df)/rdf
 }
