acf10b309718672416c4fb75429833562b40f2f1
MeganTJ21 <megan.n.taylor@vanderbilt.edu>
2022-07-25 17:03:08 -0500

    more tests

--- tests/testthat/test-resi.R
+++ tests/testthat/test-resi.R
@@ -88,19 +88,75 @@ test_that("Bayesian bootstrap is not allowed for survival models",{
 })
 
 test_that("resi produces the correct estimates", {
-  expect_equal(unname(resi(mod, nboot = 1)$estimates), c(0.36117812, -0.06733537, -0.02670248, -0.03341748, -0.00246893, 0.14980504, 0.15238719, 0.05839381, 0.01667149, 0.03610640, -0.01497171, 0.03669101, 0.29644558, 0.15047826, 0.05179287, 0.01623381), tolerance = 1e-07)
-  expect_equal(unname(resi(mod.lm, nboot = 1)$estimates[1:16]), c(0.36117812, -0.067297337, -0.026687397, -0.033398602, -0.002467535, 0.149720414, 0.152301107, 0.058360820, 0.016662071, 0.036086004, -0.014963251, 0.036616942, 0.296220354, 0.150361134, 0.051742893, 0.016116372))
-  expect_equal(unname(resi(mod.s, nboot = 1, data = data)$estimates[c(1, 6, 11, 24)]), c(0.358761703, 0.061434855, 0.007485770, 0.053752846))
-  # expect_equal(unname(resi(mod.nls, nboot = 1, data = data.nls)$estimates), c(5.6265936, 0.2487104, -1.2315892), tolerance = 1e-07)
+  expect_equal(unname(resi(mod, nboot = 1)$estimates), c(0.35982590, -0.06733537, -0.02670248, -0.03341748, -0.00246893, 0.14980504, 0.15238719,
+                                                         0.05839381, 0.01667149, 0.03610640, -0.01497171, 0.03655364, 0.29533571, 0.14991488,
+                                                         0.05159896, 0.01617303), tolerance = 1e-07)
+  expect_equal(unname(resi(mod.lm, nboot = 1)$estimates), c(0.359825898, -0.067297337, -0.026687397, -0.033398602, -0.002467535, 0.149720414, 0.152301107,
+                                                            0.058360820, 0.016662071, 0.036086004, -0.014963251, 0.036616942, 0.296220354, 0.150361134,
+                                                            0.051742893, 0.016116372))
+  expect_equal(unname(resi(mod.s, nboot = 1, data = data)$estimates[c(1, 6, 11, 24)]), c(0.35634034, 0.06143486, 0.00748577, 0.05339006), tolerance = 1e-07)
+  # expect_equal(unname(resi(mod.nls, nboot = 1, data = data.nls)$estimates), c(11.763649, 8.979576, 1.352517), tolerance = 1e-07)
   # need to figure out what is happening with nls
-  expect_equal(unname(resi(mod.surv, nboot = 1, data = data.surv)$estimates), c(0.19617550, 0.51465342, -0.08062289, 0.20010885, 0.10512266, -0.27578597, 0.0465963774, 0.1913669444, 0.0827341453), tolerance = 1e-07)
-  expect_equal(unname(resi(mod.coxph, nboot = 1, data = data.surv)$estimates), c(0.218801292, 0.131892831, -0.206640953, 0.008371707, 0.114818192, 0.197041566, 0.0000))
-  expect_equal(unname(resi(mod.hurdle, nboot = 1)$estimates[c(1, 2, 5, 8, 10)]), c(0.282492916, 0.127704891, -0.068988843, 0.026863048, 0.060397905))
-  expect_equal(unname(resi(mod.zinf, nboot = 1)$estimates[c(1, 2, 5, 8, 10)]), c(0.2388273946, 0.1189293600, -0.0701493381, -0.0348167258, -0.0346123544))
+  expect_equal(unname(resi(mod.surv, nboot = 1, data = data.surv)$estimates), c(0.19357703, 0.51465342, -0.08062289, 0.20010885, 0.10512266, -0.27578597, 0.04597918,
+                                                                                0.18883217, 0.08163828), tolerance = 1e-07)
+  expect_equal(unname(resi(mod.coxph, nboot = 1, data = data.surv)$estimates), c(0.224354226, 0.136138740, -0.213293162, 0.008641209, 0.117732151, 0.202042262, 0.000000000))
+  expect_equal(unname(resi(mod.hurdle, nboot = 1)$estimates[c(1, 2, 5, 8, 10)]), c(0.28063439, 0.12770489, -0.06898884, 0.02686305, 0.06039791), tolerance = 1e-07)
+  expect_equal(unname(resi(mod.zinf, nboot = 1)$estimates[c(1, 2, 5, 8, 10)]), c(0.23725614, 0.11892936, -0.07014934, -0.03481673, -0.03461235), tolerance = 1e-07)
   expect_equal(unname(resi(mod.gee, nboot = 10, data = data.gee)$coefficients[,'RESI']), c(0.0000, 0.4850899, 0.0000, 0.5591547, 0.2889370), tolerance = 1e-07)
   expect_equal(unname(resi(mod.geeglm, nboot = 10, data = data.gee)$coefficients[,'RESI']), c(0.0000, 0.4850899, 0.000, 0.5591547, 0.2889370), tolerance = 1e-07)
   expect_equal(unname(resi(mod.lme, nboot = 10, data = data.gee)$coefficients[,'RESI']), c(3.659090, 1.739166, 0.512371), tolerance = 1e-07)
 })
 
-# test if estimate is within confidence interval
+test_that("RESI estimates are in between the confidence limits", {
+  resi.obj = resi(mod, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  expect_true(all(resi.obj$anova$RESI >= resi.obj$anova$`2.5%`) & all(resi.obj$anova$RESI <= resi.obj$anova$`97.5%`))
+  # bayes option for glm not seeming right currently
+  # resi.obj = resi(mod, boot.method = "bayes")
+  # expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  # expect_true(all(resi.obj$anova$RESI >= resi.obj$anova$`2.5%`) & all(resi.obj$anova$RESI <= resi.obj$anova$`97.5%`))
+  resi.obj = resi(mod.lm, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  expect_true(all(resi.obj$anova[1:5, "RESI"] >= resi.obj$anova[1:5, "2.5%"]) & all(resi.obj$anova[1:5, "RESI"] <= resi.obj$anova[1:5, "RESI"]))
+  resi.obj = resi(mod.nls, data = data.nls)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  resi.obj = resi(mod.surv, data = data.surv, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  expect_true(all(resi.obj$anova$RESI >= resi.obj$anova$`2.5%`) & all(resi.obj$anova$RESI <= resi.obj$anova$`97.5%`))
+  resi.obj = resi(mod.coxph, data = data.surv, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  expect_true(all(resi.obj$anova$RESI >= resi.obj$anova$`2.5%`) & all(resi.obj$anova$RESI <= resi.obj$anova$`97.5%`))
+  resi.obj = resi(mod.hurdle, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  # resi.obj = resi(mod.hurdle, boot.method = "bayes", nboot = 500)
+  # expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  resi.obj = resi(mod.zinf, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  # resi.obj = resi(mod.gee, data = data.gee, nboot = 500)
+  # expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  resi.obj = resi(mod.geeglm, data = data.gee, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+  resi.obj = resi(mod.lme, nboot = 500)
+  expect_true(all(resi.obj$coefficients$RESI >= resi.obj$coefficients$`2.5%`) & all(resi.obj$coefficients$RESI <= resi.obj$coefficients$`97.5%`))
+})
+
+test_that("z2S.form = 2 returns same abs. RESI as Chi-sq",{
+  resi.obj = resi(mod, nboot = 1, z2S.form = 2)
+  expect_equal(abs(resi.obj$coefficients[6:7, 'RESI']), resi.obj$anova[3:4, 'RESI'], tolerance = 1e-07)
+  resi.obj = resi(mod.surv, data = data.surv, nboot = 1, z2S.form = 2)
+  expect_equal(abs(resi.obj$coefficients[2:4, 'RESI']), resi.obj$anova[1:3, 'RESI'], tolerance = 1e-07)
+  resi.obj = resi(mod.coxph, data = data.surv, nboot = 1, z2S.form = 2)
+  expect_equal(abs(resi.obj$coefficients[1:3, 'RESI']), resi.obj$anova[1:3, 'RESI'], tolerance = 1e-07)
+})
+
+test_that("t2S.form = 2 returns same abs. RESI as F", {
+  resi.obj = resi(mod.lm, nboot = 1, t2S.form = 2)
+  expect_equal(abs(resi.obj$coefficients[6:7,'RESI']), resi.obj$anova[3:4,'RESI'], tolerance = 1e-07)
+})
+
+test_that("wald.test and waldtest still consistent",{
+  expect_equal(unname(lmtest::waldtest(mod.lm, vcov = vcovHC, test = "Chisq")$Chisq[2]),
+               unname(aod::wald.test(vcovHC(mod.lm), coef(mod.lm),
+                                     Terms = 2:nrow(vcovHC(mod.lm)))$result$chi2["chi2"]))
+})
 
