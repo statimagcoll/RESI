8c878a68ed916e0b631a95c8d3c620d521d03c52
MeganTJ21 <megan.n.taylor@vanderbilt.edu>
2022-08-02 21:24:49 -0500

    documentation

--- R/Anova.resi.R
+++ R/Anova.resi.R
@@ -1,5 +1,3 @@
-# 'could not find function'
-
 #' Anova method for resi objects
 #'
 #' After running the \code{\link{resi}} function on a fitted model, this function can be used to print the Anova-style table component. If the resi function was run with the `store.boot = TRUE` option to store the full matrix of bootstrapped estimates, the user can specify a different alpha level for this function's confidence intervals.
@@ -7,6 +5,15 @@
 #' @param alpha an optional new specification for the confidence level. Can be vector-valued
 #' @param ... ignored
 #' @return Returns an `anova` object containing the computed Anova-style table
+#' @examples
+#' # fit a model
+#' mod = lm(charges ~ bmi + sex, data = RESI::insurance)
+#'
+#' # run resi with the store.boot = TRUE option
+#' resi.obj = resi(mod, nboot = 100, store.boot = TRUE, alpha = 0.01)
+#'
+#' # run Anova, specifying a different alpha level if desired
+#' car::Anova(resi.obj, alpha = 0.05)
 #' @export
 Anova.resi <- function(object, alpha = NULL, ...){
   if(is.null(object$anova)){
@@ -26,7 +33,7 @@ Anova.resi <- function(object, alpha = NULL, ...){
     }
     else{
       output = object$anova[,1:(which(colnames(object$anova) == 'RESI'))]
-      CIs = apply(object$boot.results[,(ncol(object$boot.results)-nrow(object$anova)+2):ncol(object$boot.results)], 2,  quantile, probs = c(alpha/2, 1-alpha/2), na.rm = TRUE)
+      CIs = apply(object$boot.results[,(ncol(object$boot.results)-nrow(object$anova)+1):ncol(object$boot.results)], 2,  quantile, probs = c(alpha/2, 1-alpha/2), na.rm = TRUE)
       CIs = t(CIs)
       output[1:nrow(CIs), c(paste(alpha/2*100, '%', sep=''), paste((1-rev(alpha)/2)*100, '%', sep=''))] = CIs
     }
@@ -43,6 +50,15 @@ Anova.resi <- function(object, alpha = NULL, ...){
 #' @param ... ignored
 #' @return Returns an `anova` object containing the computed Anova-style table
 #' @details The resi function uses the car::Anova function to compute the Anova table.
+#'@examples
+#' # fit a model
+#' mod = lm(charges ~ bmi + sex, data = RESI::insurance)
+#'
+#' # run resi with the store.boot = TRUE option
+#' resi.obj = resi(mod, nboot = 100, store.boot = TRUE, alpha = 0.01)
+#'
+#' # run anova, specifying a different alpha level if desired
+#' anova(resi.obj, alpha = 0.05)
 #' @export
 anova.resi <- function(object, alpha = NULL, ...){
   if(is.null(object$anova)){
@@ -62,7 +78,7 @@ anova.resi <- function(object, alpha = NULL, ...){
     }
     else{
       output = object$anova[,1:(which(colnames(object$anova) == 'RESI'))]
-      CIs = apply(object$boot.results[,(ncol(object$boot.results)-nrow(object$anova)+2):ncol(object$boot.results)], 2,  quantile, probs = c(alpha/2, 1-alpha/2), na.rm = TRUE)
+      CIs = apply(object$boot.results[,(ncol(object$boot.results)-nrow(object$anova)+1):ncol(object$boot.results)], 2,  quantile, probs = c(alpha/2, 1-alpha/2), na.rm = TRUE)
       CIs = t(CIs)
       output[1:nrow(CIs), c(paste(alpha/2*100, '%', sep=''), paste((1-rev(alpha)/2)*100, '%', sep=''))] = CIs
     }
