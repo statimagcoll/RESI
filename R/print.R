9af568555427e7019c4f9e625d8684f8b7a5a666
MeganTJ21 <megan.n.taylor@vanderbilt.edu>
2023-01-13 16:50:27 -0600

    added line for number of successful bootstraps in nls

--- R/print.R
+++ R/print.R
@@ -49,6 +49,8 @@ print.resi <- function(x, digits = max(3L, getOption("digits") - 3L), ...) {
   if (x$boot.method == "nonparam") cat("\n2. Confidence intervals (CIs) constructed using", x$nboot,"non-parametric bootstraps. \n")
   if (x$boot.method == "bayes") cat("\n2. Credible intervals constructed using", x$nboot,"Bayesian bootstraps. \n")
   # if(nzchar(mess <- naprint(x$na.action))) cat("  (",mess, ")\n", sep = "")
+  # report number of failed bootstraps for nls model
+  if (!(is.null(x$nfail))) cat("3. The bootstrap was successful in", x$nboot - x$nfail, "out of ", x$nboot, "attempts. \n" )
 
   invisible(x)
 }
