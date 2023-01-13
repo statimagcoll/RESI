b07de174baf31206392928b8b4d37a8fffca3458
MeganTJ21 <megan.n.taylor@vanderbilt.edu>
2023-01-13 16:50:02 -0600

    fixed error in the denominator, updated documentation

--- R/f2S.R
+++ R/f2S.R
@@ -4,19 +4,18 @@
 #' Vector arguments are accepted. If different length arguments are passed they are dealt with in the usual way of R.
 #' @param f The F statistic for the parameter of interest.
 #' @param df Number of degrees of freedom of the F statistic.
-#' @param rdf Model residual degrees of freedom or number of independent samples.
+#' @param rdf Model residual degrees of freedom.
+#' @param n Number of independent samples.
 #' @details The formula for converting an F statistic to S is:
 #'
-#' \eqn{ S = \sqrt(max(0, (f * df * (rdf - 2)/rdf - 1)/rdf))}
+#' \eqn{ S = \sqrt(max(0, (f * df * (rdf - 2)/rdf - 1)/n))}
 #'
 #' The estimator is derived by setting the statistic equal to the expected value of
-#' the test statistic and solving for S. A modification of dividing by the residual degrees
-#' of freedom rather than the total sample size is made based on the results of
-#' simulations that showed this adjustment results in smaller finite-sample bias.
+#' the test statistic and solving for S.
 #'
 #' @return Returns a scalar or vector argument of the the robust effect size index estimate.
-#' # to obtain example F values, first fit a glm
-#' mod = glm(charges ~ region * age + bmi + sex, data = RESI::insurance)
+#' # to obtain example F values, first fit a lm
+#' mod = lm(charges ~ region * age + bmi + sex, data = RESI::insurance)
 #'
 #' # run Anova, using a robust variance-covariance function
 #' # get the F values and Df values
@@ -24,9 +23,9 @@
 #' dfs = car::Anova(mod, vcov. = sandwich::vcovHC)$Df
 #'
 #' # get RESI estimates
-#' f2S(fs, df = dfs, rdf = mod$df.residual)
+#' f2S(fs, df = dfs, rdf = mod$df.residual, n = nrow(RESI::insurance))
 #' @export
-f2S <- function(f, df, rdf){
-  S = (f*df*(rdf-2)/rdf - df)/rdf
+f2S <- function(f, df, rdf, n){
+  S = (f*df*(rdf-2)/rdf - df)/n
   sqrt(ifelse(S<0, 0, S))
 }
